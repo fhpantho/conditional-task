baaf0a1aaec77728bae3ce4c4db25cd3434e80ab
Fahim <fahim1020pantho@gmail.com>
2025-08-01 23:47:32 +0600

    1st task is added

---
+++ 1 Free Drinks.js
@@ -0,0 +1,16 @@
+/**
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+let cokePrice = 600;
+if(cokePrice >= 500){
+    console.log("the coke is free");
+}
+else
+{
+    console.log("the coke is 30tk");
+}
